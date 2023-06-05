914d922396a3980c57e4f5ec4a7989a23c3acf1d
root <root@DESKTOP-O6IMPT5>
2023-06-04 22:56:10 -0700

    Created client and server scripts to run MPQUIC

--- clientScript.sh
+++ clientScript.sh
@@ -1,3 +1,3 @@
 cd ..
 cd quiche/
-cargo run --bin quiche-client -- --no-verify https://127.0.0.1:4433/example.txt
+cargo run --bin quiche-client -- --no-verify https://10.0.0.2:4433/example.txt
--- serverScript.sh
+++ serverScript.sh
@@ -1,3 +1,3 @@
 cd ..
 cd quiche/
-/lib/linux-tools/5.15.0-73-generic/perf stat cargo run --bin quiche-server -- --cert apps/src/bin/cert.crt --key apps/src/bin/cert.key --root apps/src/bin
+/lib/linux-tools/5.15.0-73-generic/perf stat cargo run --bin quiche-server -- --cert apps/src/bin/cert.crt --key apps/src/bin/cert.key --root apps/src/bin --listen 10.0.0.2:4433
