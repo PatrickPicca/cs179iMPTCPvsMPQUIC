c8e25f6b476ed1dd119b15d9ebae8c79745bba12
wpicc001 <wpicc001@node0.piccamininets2.kkprojects-pg0.utah.cloudlab.us>
2023-06-04 21:33:56 -0600

    Created properly working path loss switch connection.

--- basic5Loss.py
+++ basic5Loss.py
@@ -11,14 +11,23 @@ class MyTopo(Topo):
     def build(self):
         client = self.addHost('client')
         server = self.addHost('server')
-        
-        self.addLink(client,server)
+
+        switch1 = self.addSwitch('switch1', listenPort=6634)  # Set the OpenFlow port for switch1
+        switch2 = self.addSwitch('switch2', listenPort=6634)
+
+        self.addLink(client, switch1)
+        self.addLink(switch2, server)
+        self.addLink(switch1, switch2, cls=TCLink, loss=5)
 
 def create_topology():
     topo = MyTopo()
     controller = RemoteController('c0', ip='127.0.0.1', port=6653)  # Specify the Ryu controller
     net = Mininet(topo=topo, controller=controller)
     net.start()
+
+    # Add the command to set the switch flow
+    net.get('switch1').cmd('ovs-ofctl add-flow switch1 action=normal')
+    net.get('switch2').cmd('ovs-ofctl add-flow switch2 action=normal')
 
     CLI(net)
     net.stop()
--- basic5LossScript.sh
+++ basic5LossScript.sh
@@ -3,5 +3,5 @@
 # Run setup script that creates the client and server terminal.
 # NOT FINISHED: uploading and storing data obtained to github.
 
-sudo mn --custom=mytopo.py --topo=mytopo --controller=none --mac --pre=runClients.sh 
+sudo ./basic5Loss.py
 
