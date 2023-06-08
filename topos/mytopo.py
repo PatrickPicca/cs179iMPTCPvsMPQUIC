822548b4e9ce68eaae001f4f728d2445b8b5372f
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-06-07 21:46:16 -0700

    saving progress

--- topos/mytopo.py
+++ topos/mytopo.py
@@ -16,11 +16,12 @@ class MyTopo(Topo):
 
 def create_topology():
     topo = MyTopo()
-    controller = RemoteController('c0', ip='127.0.0.1', port=6653)  # Specify the Ryu controller
+    controller = RemoteController('c0', ip='10.0.0.10', port=6653)  # Specify the Ryu controller
     net = Mininet(topo=topo, controller=controller)
-    net.start()
-    CLI(net)
-    net.stop()
+    #net.addNAT().configDefault()
+    #net.start()
+    #CLI(net)
+    #net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
