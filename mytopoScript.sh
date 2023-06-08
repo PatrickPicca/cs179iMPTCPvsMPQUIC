f7e102198a5544222b70f50030548c5c6a121878
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-06-08 14:14:08 -0700

    Updated

--- mytopoScript.sh
+++ mytopoScript.sh
@@ -3,5 +3,7 @@
 # Run setup script that creates the client and server terminal.
 # NOT FINISHED: uploading and storing data obtained to github.
 
-# sudo mn --custom mytopo.py --topo mytopo --xterm 
-sudo ./topos/mytopo.py
+sudo mn --custom ./topos/newTopo --topo=mytopo --nat --mac
+
+#sudo mn --custom ./topos/mytopo.py --topo=mytopo
+#sudo ./topos/mytopo.py
--- topos/basic5Loss.py
+++ topos/basic5Loss.py
@@ -7,31 +7,17 @@ from mininet.link import TCLink
 from mininet.node import RemoteController
 from mininet.cli import CLI
 
-class MyTopo(Topo):
+class CustomTopo(Topo):
     def build(self):
-        client = self.addHost('client')
-        server = self.addHost('server')
+        client = self.addHost('client', ip='10.0.0.1')
+        server = self.addHost('server', ip='10.0.0.2')
 
-        switch1 = self.addSwitch('switch1', listenPort=6634)  # Set the OpenFlow port for switch1
-        switch2 = self.addSwitch('switch2', listenPort=6634)
+        switch1 = self.addSwitch('switch1')  
+        switch2 = self.addSwitch('switch2')
 
         self.addLink(client, switch1)
         self.addLink(switch2, server)
         self.addLink(switch1, switch2, cls=TCLink, loss=5)
 
-def create_topology():
-    topo = MyTopo()
-    controller = RemoteController('c0', ip='10.0.0.10', port=6653)  # Specify the Ryu controller
-    net = Mininet(topo=topo, controller=controller)
-    net.start()
-
-    # Add the command to set the switch flow
-    net.get('switch1').cmd('ovs-ofctl add-flow switch1 action=normal')
-    net.get('switch2').cmd('ovs-ofctl add-flow switch2 action=normal')
-
-    CLI(net)
-    net.stop()
-
-if __name__ == '__main__':
-    setLogLevel('info')
-    create_topology()
+topo = CustomTopo()
+topos = {'mytopo': CustomTopo}
