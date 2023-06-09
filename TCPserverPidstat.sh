a858feabfda81e051b63846e84155a8aeff85b52
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-06-09 11:42:53 -0700

    Updated scripts

---
+++ TCPserverPidstat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+target_command="python3 server.py"
+mkdir -p ./TCPstats
+output_directory="./TCPstats"
+file_count=$(find "$output_directory" -type f | wc -l)
+echo "$file_count"
+
+while true; do
+  # Check if the process has started
+  if pgrep -f "$target_command" >/dev/null; then
+    # Get process statistics
+    echo "Found targeted process with PID: $(pgrep -f "$target_command")"
+    thePID=$(pgrep -f -n "$target_command")
+    echo "pidstat -p "$thePID" 1 >> ./TCPstats/serverPidstatOut_$file_count.log"
+    pidstat -p "$thePID" 1 >> ./$output_directory/serverPidstatOut_$file_count.log
+    #pidstat -p 31628 1 >> ./$output_directory/serverPidstatOut_$file_count.log
+    echo "Done recording pidstat"
+    break
+  fi
+  sleep 1
+done
+
--- serverScript.sh
+++
@@ -1,4 +0,0 @@
-cd ..
-cd quiche/
-#cargo run --bin quiche-server -- --cert apps/src/bin/cert.crt --key apps/src/bin/cert.key --root apps/src/bin --listen 10.0.0.2:4433
-perf stat cargo run --bin quiche-server -- --cert apps/src/bin/cert.crt --key apps/src/bin/cert.key --root apps/src/bin --listen 10.0.0.2:4433
