fb07dffae1e723b41b605ec77de1965fd36fd6eb
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-06-09 08:23:38 -0700

    Updated scripts to run perf stat

--- clientScript.sh
+++ clientScript.sh
@@ -1,4 +1,4 @@
 cd ..
 cd quiche/
-cargo run --bin quiche-client -- --no-verify https://10.0.0.2:4433/example.txt
-#/lib/linux-tools/5.15.0-73-generic/perf stat cargo run --bin quiche-client -- --no-verify https://10.0.0.2:4433/example.txt
+#cargo run --bin quiche-client -- --no-verify https://10.0.0.2:4433/example.txt
+perf stat cargo run --bin quiche-client -- --no-verify https://10.0.0.2:4433/example.txt
