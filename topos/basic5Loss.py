e933546c7e213d1d5011fe5e46ca5658417a949c
root <root@ubuntu.myguest.virtualbox.org>
2023-06-09 19:34:26 -0700

    Update topologies basic

--- topos/basic5Loss.py
+++ topos/basic5Loss.py
@@ -9,15 +9,16 @@ from mininet.cli import CLI
 
 class CustomTopo(Topo):
     def build(self):
+        switch1 = self.addSwitch('switch1')
+        switch2 = self.addSwitch('switch2')
+
         client = self.addHost('client', ip='10.0.0.1')
         server = self.addHost('server', ip='10.0.4.1')
 
-        switch1 = self.addSwitch('switch1', ip='10.0.1.1')  
-        switch2 = self.addSwitch('switch2', ip='10.0.2.1')
-
-        self.addLink(client, switch1, delay='110ms')
-        self.addLink(switch2, server, delay='110ms')
-        self.addLink(switch1, switch2, cls=TCLink, delay='110ms', loss=5)
+        self.addLink(client, switch1, cls=TCLink, delay='20ms')
+        self.addLink(switch1, switch2, cls=TCLink, delay='20ms', loss=5) #this
+        self.addLink(switch2, server, cls=TCLink, delay='20ms') #this
+        
 
 topo = CustomTopo()
 topos = {'mytopo': CustomTopo}
