35d2f2d9ae7e7c54951c5c2ca7689fd2862b0c5c
root <root@ubuntu.myguest.virtualbox.org>
2023-06-10 05:03:44 -0700

    All logs for newTopo generated

--- MPQUICserverPidstat.sh
+++ MPQUICserverPidstat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-target_command="target/debug/quiche-server --cert apps/src/bin/cert.crt --key apps/src/bin/cert.key --root apps/src/bin --listen 10.0.0.2:4433"
+target_command="target/debug/quiche-server --cert apps/src/bin/cert.crt --key apps/src/bin/cert.key --root apps/src/bin --listen 10.0.4.1:4433"
 mkdir -p ./MPQUICstats
 output_directory="./MPQUICstats"
 file_count=$(find "$output_directory" -type f | wc -l)
@@ -12,7 +12,7 @@ while true; do
     # Get process statistics
     echo "Found targeted process with PID: $(pgrep -f "$target_command")"
     thePID=$(pgrep -f -n "$target_command")
-    pidstat -p "$thePID"  1 >> ./MPQUICstats/serverPidstatOut_$file_count.log
+    pidstat -p "$thePID" 1 >> ./MPQUICstats/serverPidstatOut_$file_count.log
     echo "Done recording pidstat"
     break
   fi
